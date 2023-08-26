bb44c3c69e4bbf5d4dfaa77584524f9477af3fad
Amjadayash <92536475+Amjadayash@users.noreply.github.com>
2023-08-27 00:11:13 +0300

    التكليف الثاني للأرقام العشوائية
    تحت إشراف د/حسين الخضر

--- main.dart
+++ main.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: unused_element
+import 'dart:math';
 
 import 'package:flutter/material.dart';
 
@@ -9,16 +9,31 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'التكليف الاول',
+      title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(
-            seedColor: const Color.fromARGB(255, 224, 161, 43)),
+        // This is the theme of your application.
+        //
+        // TRY THIS: Try running your application with "flutter run". You'll see
+        // the application has a blue toolbar. Then, without quitting the app,
+        // try changing the seedColor in the colorScheme below to Colors.green
+        // and then invoke "hot reload" (save your changes or press the "hot
+        // reload" button in a Flutter-supported IDE, or press "r" if you used
+        // the command line to start the app).
+        //
+        // Notice that the counter didn't reset back to zero; the application
+        // state is not lost during the reload. To reset the state, use hot
+        // restart instead.
+        //
+        // This works for code too, not just values: Most code changes can be
+        // tested with just a hot reload.
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'م/ أمجد عبدالله عياش'),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
@@ -32,58 +47,48 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+int ss = 0;
 
+class _MyHomePageState extends State<MyHomePage> {
+  // ignore: prefer_final_fields
+  int _counter = 0;
+  int y = 0;
   void _incrementCounter() {
     setState(() {
-      _counter++;
-    });
-  }
+      //_counter++;
 
-  void _incrementCounter1() {
-    setState(() {
-      _counter--;
+      _counter = Random().nextInt(100);
+      y = _counter * 10;
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          //shadowColor: const Color.fromARGB(5, 172, 221, 57),
-          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-
-          title: Text(widget.title),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              const Text(
-                'بإمكانك زيادة أو نقصان العدد عن طريق الأزرار الذي بالأسفل',
-              ),
-              Text(
-                '$_counter',
-                style: Theme.of(context).textTheme.headlineMedium,
-              ),
-            ],
-          ),
-        ),
-        // This trailing comma makes auto-formatting nicer for build methods.
-        bottomNavigationBar: BottomAppBar(
-            child: Row(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text("$y"),
+      ),
+      body: Center(
+        child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            IconButton(
-              icon: const Icon(Icons.add),
-              onPressed: _incrementCounter,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
             ),
-            IconButton(
-                icon: const Icon(Icons.minimize),
-                onPressed: _incrementCounter1),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
           ],
-        )));
+        ),
+      ),
+
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Random Number',
+        child: const Icon(Icons.refresh),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
   }
 }
